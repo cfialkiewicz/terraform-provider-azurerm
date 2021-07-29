3ad4573574d3fca0314f5b9410af52f44e38cde8
jackofallops <ste@hashicorp.com>
2021-07-29 16:03:10 +0100

    copy paste error

--- azurerm/internal/services/appservice/linux_web_app_data_source_test.go
+++ azurerm/internal/services/appservice/linux_web_app_data_source_test.go
@@ -11,7 +11,7 @@ import (
 type LinuxWebAppDataSource struct{}
 
 func TestAccLinuxWebAppDataSource_complete(t *testing.T) {
-	data := acceptance.BuildTestData(t, "data.azurerm_windows_web_app", "test")
+	data := acceptance.BuildTestData(t, "data.azurerm_linux_web_app", "test")
 	d := LinuxWebAppDataSource{}
 
 	data.DataSourceTest(t, []acceptance.TestStep{
@@ -28,9 +28,9 @@ func (LinuxWebAppDataSource) complete(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 %s
 
-data azurerm_windows_web_app test {
-  name                = azurerm_windows_web_app.test.name
-  resource_group_name = azurerm_windows_web_app.test.resource_group_name
+data azurerm_linux_web_app test {
+  name                = azurerm_linux_web_app.test.name
+  resource_group_name = azurerm_linux_web_app.test.resource_group_name
 }
 
 `, LinuxWebAppResource{}.basic(data)) // TODO - Update this to l.complete when that config is written
